81e96337c9dca3c5bfc9ffe4bf717d3ec9025511
Lawson C <lawson.conallin@gmail.com>
2025-05-05 13:29:55 +1000

    fix the program still sleeping when skipping the animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,26 @@
 // Index animations
 async function typing_animation() {
-    //await sleep(3000);
+    //await typesleep(3000);
     await type("|CONNECTING");
     await type_speed(".......", 300);
     await type("|")
     await type("Connection Successful. ");
-    await sleep(500);
+    await typesleep(500);
     await type("Starting Transmission.")
-    await sleep(100);
-    await type("|FROM: Lawson|TO: "+ip)
+    await typesleep(100);
+    await type("||FROM: Lawson|TO: "+ip)
     await type_speed("|---------------------------------------------|", 10);
-    await sleep(300);
-    await type("|Hi there, "+ip+".||")
+    await typesleep(500);
+    await type("Hi there, "+ip+".||")
     await type("My name is Lawson.||")
-    await sleep(100);
+    await typesleep(100);
     await type("I'm a highschool student living in Australia. I have an interest programming in general, home automation, electronics and gaming (ofc). ")
     await type("I have a ");
     await link("https://youtube.com/notlawson", "Youtube");
     await type(" channel, where I upload mostly gaming content, Rocket League and Helldivers 2.|| ")
-    await sleep(500);
+    await typesleep(500);
     await type("What have I done so far? ");
-    await sleep(300);
+    await typesleep(300);
     await type("Well, let's see:| ");
     await list([
         "I've won the 2024 Premiers Coding Challenge",
@@ -29,11 +29,11 @@ async function typing_animation() {
         "I made this website, I guess."
     ]);
     await type("|But yeah. Welcome to my website.|Don't worry, the rest of the website is better than this. Look, I'll even show you a nav bar.||");
-    await sleep(1000);
+    await typesleep(1000);
     document.getElementById("navbar").removeAttribute("hidden");
-    await sleep(300);
+    await typesleep(300);
     await type("Tada!|| ");
-    await sleep(300);
+    await typesleep(300);
     await type("Now, go check out my projects and abitions. The Assets page allows you to view the contents of each file used in this website, in case you want to replicate what I've done here.||")
     await type("       - Lawson");
     await type_speed("|---------------------------------------------|", 10);
