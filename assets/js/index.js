abaa0e966f8aad56aa408a391adcaeb926f54983
Lawson C <lawson.conallin@gmail.com>
2025-08-04 12:18:09 +1000

    change the headings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,8 @@
 // Index animations
 async function typing_animation() {
-    await heading("Home");
-    await typesleep(1000);
     var greeting = ["Hello", "Hiya", "Welcome", "Greetings", "你好", "こんにちは", "Bonjour", "Haileo", "Ciao", "Witam", "Hola", "Salutamu"][Math.random() * 12 | 0];
-    await type(greeting + ", " + ip + ", to my personal website!|");
-    await type("I'm NotLawson, a 15-year-old student from Queensland, Australia.||");
+    await heading(greeting + ", " + ip + ", to my personal website!|");
+    await typesleep("I'm NotLawson, a 15-year-old student from Queensland, Australia.||");
     await typesleep(500);
     await type("About me")
     await type_speed("..... ", 100);
