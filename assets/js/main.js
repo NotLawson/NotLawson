5b61c47cfc06243c0f313dc49fc6aae8c3683d59
Lawson C <lawson.conallin@gmail.com>
2025-11-01 18:22:01 +1000

    try caddy templates instead

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ document.addEventListener('keydown', function (event) {
 // IP Grabber
 var ip;
 async function iploader() {await fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data => {ip = data.ip;});console.log("Your IP is: "+ip);}
-iploader();
+//iploader();
 
 // Param Reader
 const urlParams = new URLSearchParams(window.location.search);
