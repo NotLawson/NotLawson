7b9dc10b1458b699432f661ce7bb510d41a04103
Lawson C <lawson.conallin@gmail.com>
2025-11-02 09:13:10 +1000

    changes to the main page

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,31 +6,20 @@ async function typing_animation() {
     await typesleep("I'm NotLawson, a 15-year-old student from Queensland, Australia.||");
     await typesleep(500);
     await type("About me")
-    await type_speed("..... ", 100);
+    await type_speed("..... ", 300);
     await typesleep(500);
-    await type("Let's see.");
+    await type("Let's see.|");
     await typesleep(500);
-    await type("|- I enjoy programming and learning new things.|");
-    await type("- I started coding when I was around 10, and I know Python, JavaScript, and Arduino.|");
-    await type("- I like messing around with electronics and home automation.|");
-    await type("- I've been using Home Assistant for around 5 years now, and have quite a few funky automations.||");
+    await type(" - I love computers (obviously) and electronics|");
     await typesleep(500);
-    await type("I also enjoy gaming, especially Rocket League and Helldiver II.|");
-    await type("I upload (or don't) to my ");
-    await link("https://www.youtube.com/@NotLawson", "YouTube channel");
-    await type(" and stream on ");
-    await link("https://www.twitch.tv/notlawson8129", "Twitch");
-    await type("|I sometimes do some edits and stuff around the things I'm watching, like Arcane, which I also upload there.||");
+    await type(" - I enjoy gaming, specifically Rocket League and Helldivers II.|");
     await typesleep(500);
-    await type("I play Division 1 Basketball in my age group, and have a inter-school eSport Rocket League Championship.||");
+    await type(" - I enjoy making edits and AMVs, specifically for Arcane (deal with it) on my ");
+    await link("https://www.youtube.com/@NotLawson", "YouTube channel.");
     await typesleep(500);
-    await type("Uhh, but that's about it really. Hit me up at |");
-    await link("mailto:lawson.conallin@gmail.com", "lawson.conallin@gmail.com");
-    await type(" if you need anything, or my Discord (NotLawson).||")
+    await type("||Thats about it. Have fun poking around my website, I guess.||");
     await typesleep(500);
-    await type("Have fun poking around my website, I guess?||");
-    await typesleep(500);
-    await type("   - NotLawson");
+    await type(" - NotLawson");
     SKIP = true;
     await type("||(top tip: press 's' to skip the animations on any page.)|")
 } 
